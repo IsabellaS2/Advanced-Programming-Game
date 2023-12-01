201c81de44ae2a76d4e9daa27fac60df85abacc6
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2023-12-01 16:56:54 +0000

    Remove cpp files and add JavaScript files

--- MenuFiles/menu.h
+++
@@ -1,6 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-int menuMain(); 
-
-#endif
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "MenuFiles/menu.h"
-
-int main() {
-    std::cout << "This is the main cat file!" << std::endl;
-    menuMain();
-    return 0;
-}
