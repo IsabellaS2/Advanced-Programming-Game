77677ed1074ac72fec37d7ed23652454d0ca3a24
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2023-12-05 09:41:36 +0000

    Making one cat move the board

--- index.js
+++ index.js
@@ -3,30 +3,11 @@ class GameBoard {
     this.container = container;
     this.numberOfDivs = numberOfDivs;
     this.resultText = resultText;
-    this.createBoard();
-    this.boxNumbers(); 
-    this.diceRoller = new DiceRoller(this.resultText);
+    this.cat = new Cat(this.container, this.numberOfDivs); // Create an instance of Cat
+    this.diceRoller = new DiceRoller(this.resultText, this.catMove.bind(this)); // Pass the catMove method
   }
-
-  createBoard() {
-    for (let i = 1; i <= this.numberOfDivs; i++) {
-      const newDiv = $("<div>", {
-        id: "box-" + i,
-        class: "box",
-      });
-      this.container.append(newDiv);
-    }
-  }
-
-  boxNumbers() {
-    let boxes = $(".box"); 
-    boxes.each((i, box) => {
-      if (String(i).length == 1 || (String(i).length == 2 && Number(String(i)[0]) % 2 == 0)) {
-        $(box).html(100 - i);
-      } else {
-        $(box).html(Number(`${9 - Number(String(i)[0])}${String(i)[1]}`) + 1);
-      }
-    });
+  catMove(steps) {
+    this.cat.move(steps);
   }
 }
 
@@ -34,7 +15,7 @@ class DiceRoller {
   constructor(resultText, onRollCallback) {
     this.resultText = resultText;
     this.button = $("#roll-dice-btn");
-    this.diceResult = 0; 
+    this.diceResult = 0;
     this.onRollCallback = onRollCallback;
 
     this.button.on("click", () => this.roll());
@@ -43,7 +24,7 @@ class DiceRoller {
   roll() {
     this.diceResult = this.generateRandomNumber();
     this.displayResult();
-    if (typeof this.onRollCallback === 'function') {
+    if (typeof this.onRollCallback === "function") {
       this.onRollCallback(this.diceResult);
     }
   }
@@ -57,20 +38,52 @@ class DiceRoller {
   }
 }
 
+class Cat {
+  constructor(container, numberOfDivs) {
+    this.container = container;
+    this.numberOfDivs = numberOfDivs;
+    this.createCat();
+  }
+
+  createCat() {
+    this.catDiv = $("<div>", {
+      id: "black-cat",
+      class: "players",
+    });
+
+    this.container.append(this.catDiv);
+  }
+
+  move(steps) {
+    const currentPosition = parseInt(this.catDiv.html()) || 1; // Get the current position
+
+    // Calculate the new position
+    let newPosition = currentPosition + steps;
+    if (newPosition > this.numberOfDivs) {
+      newPosition = this.numberOfDivs;
+    }
+
+    // Get the box number at the new position
+    let newBoxNumber = newPosition;
+
+    // Move the cat div to the new position
+    this.catDiv.html(newBoxNumber);
+
+    // Move the cat div visually
+    this.container.find(`#${this.catDiv.attr("id")}`).animate(
+      {
+        top: $(`#box-${newBoxNumber}`).position().top + "px",
+        left: $(`#box-${newBoxNumber}`).position().left + "px",
+      },
+      500
+    ); // Adjust the animation duration as needed
+  }
+}
+
 $(document).ready(function () {
-  const gameBoard = new GameBoard($("#game-board"), 100, $("#dice-roll-result"));
-  const onRollCallback = function (diceResult) {
-    console.log("Stored Dice Roll Result:", diceResult);
-  };
-  const diceRoller = new DiceRoller($("#dice-roll-result"), onRollCallback);
+  const gameBoard = new GameBoard(
+    $("#game-board"),
+    100,
+    $("#dice-roll-result")
+  );
 });
-
-
-
-
-// const playerName = localStorage.getItem("playerName");
-// if (playerName) {
-//   $("#player-turn").text(playerName + "'s Turn");
-// }
-
-
