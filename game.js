cf9df86af8d62a0b9678c15e5db296958337e3cd
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2023-12-08 14:55:42 +0000

    Add strings and streams

--- game.js
+++ game.js
@@ -67,11 +67,11 @@ class Cat {
 }
 
 class GameBoard {
-  constructor(gameBoardContainer, numberOfDivs, resultText) {
-    this.gameBoardContainer = gameBoardContainer;
+  constructor(container, numberOfDivs, resultText) {
+    this.container = container;
     this.numberOfDivs = numberOfDivs;
     this.resultText = $("#" + resultText.attr("id"));
-    this.catCollecttion = [];
+    this.cats = [];
     this.currentCatIndex = 0;
 
     // Create the DiceRoller instance and bind the event handler
@@ -81,17 +81,94 @@ class GameBoard {
     );
     $("#roll-dice-btn").on("click", diceRoller.performRoll.bind(diceRoller));
     this.diceRoller = diceRoller;
+
+    // Adding stream images
+    this.addImageToBoard("./assets/streams/stream-image1.png", 45, {
+      topOffset: 80,
+      leftOffset: 70,
+      height: 160,
+      width: 150,
+    });
+
+    this.addImageToBoard("./assets/streams/stream-image2.png", 5, {
+      topOffset: 40,
+      leftOffset: 65,
+      height: 160,
+      width: 150,
+    });
+
+    this.addImageToBoard("./assets/streams/stream-image3.png", 68, {
+      topOffset: 65,
+      leftOffset: 70,
+      height: 160,
+      width: 150,
+    });
+
+    this.addImageToBoard("./assets/streams/stream-image4.png", 29, {
+      topOffset: 0,
+      leftOffset: 40,
+      height: 180,
+      width: 200,
+    });
+
+    this.addImageToBoard("./assets/streams/stream-image5.png", 34, {
+      topOffset: 30,
+      leftOffset: 70,
+      height: 180,
+      width: 200,
+    });
+
+    // Adding string images
+    this.addImageToBoard("./assets/strings/string-image1.png", 50, {
+      topOffset: 30,
+      leftOffset: 100,
+      height: 180,
+      width: 200,
+    });
+  }
+
+  addImageToBoard(imageFilename, nearBoxNumber, options) {
+    options = options || {};
+    const {
+      topOffset = 0,
+      leftOffset = 0,
+      height = 100,
+      width = 100,
+    } = options;
+
+    const image = $("<img>", {
+      src: imageFilename,
+      alt: "Image",
+      class: "custom-image", // Add a class for styling
+    });
+
+    // Set the position based on the desired box position
+    const boxOffset = $(`#box-${nearBoxNumber}`).offset();
+    image.css({
+      position: "absolute",
+      top: boxOffset.top - topOffset,
+      left: boxOffset.left - leftOffset,
+      height: height + "px",
+      width: width + "px",
+      transform: "translate(-50%, -50%)",
+      zIndex: 1,
+    });
+
+    // Generate a unique ID for each image based on the filename
+    const uniqueID = imageFilename.replace(/\W/g, "_").replace(/\./g, "_");
+
+    image.attr("id", uniqueID);
+    this.container.append(image);
   }
 
   addCatToBoard(catColor) {
-    const cat = new Cat(this.gameBoardContainer, this.numberOfDivs, catColor);
-    this.catCollecttion.push(cat);
+    const cat = new Cat(this.container, this.numberOfDivs, catColor);
+    this.cats.push(cat);
   }
 
-  moveCurrentCat(distanceToMoveCat) {
-    const currentCat = this.catCollecttion[this.currentCatIndex];
-    currentCat.moveToNewPosition(distanceToMoveCat);
-    this.currentCatIndex =
-      (this.currentCatIndex + 1) % this.catCollecttion.length;
+  moveCurrentCat(steps) {
+    const currentCat = this.cats[this.currentCatIndex];
+    currentCat.moveToNewPosition(steps);
+    this.currentCatIndex = (this.currentCatIndex + 1) % this.cats.length;
   }
 }
